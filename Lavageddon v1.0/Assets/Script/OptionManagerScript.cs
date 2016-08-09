822c6a0a1b88da70b727b49207c67acfa15a1bd8
Zac <zac.molyneux@hotmail.com>
2016-08-09 10:28:42 +1000

    added OptionManagerScript

---
+++ Lavageddon v1.0/Assets/Script/OptionManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OptionManagerScript : MonoBehaviour
+{
+    public Button[] buttons;
+    public Text[] texts;
+    //public GameObject[] buttons;
+    //public GameObject[] text;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " was selected");
+    }
+
+
+    void Start()
+    {
+        //button[0].OnSelect
+    }
+
+    void Update()
+    {
+        //if()
+    }
+}
