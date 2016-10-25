74fc32d02850b17a4fa0f1a4eb6f07ccc4d8c8d4
Roy Kirk <ricotalzier@gmail.com>
2016-10-26 10:52:19 +1100

    prefab update

--- Lavageddon v1.0/Assets/Script/PlayerMovement.cs
+++ Lavageddon v1.0/Assets/Script/PlayerMovement.cs
@@ -679,6 +679,38 @@ public class PlayerMovement : MonoBehaviour {
 
     }
 
+    void Awake()
+    {
+        DynamicVariables DV = playerManager.GetComponent<DynamicVariables>();
+        Rigidbody rb = body.transform.parent.GetComponent<Rigidbody>();
+
+        //PLAYER RELATED VALUES
+        jumpForce = (DV.PlayerRelated[0] * 10);
+        sensitivityX = DV.PlayerRelated[1];
+        sensitivityY = DV.PlayerRelated[2];
+        rb.mass = DV.PlayerRelated[3];
+        submergedMinTime = DV.PlayerRelated[4];
+        if (DV.PlayerRelated[5] == 1)
+        {
+            submergeAccumulate = true;
+        }
+        else
+        {
+            submergeAccumulate = false;
+        }
+        movementSpeed = DV.PlayerRelated[7];
+
+        //WEAPON RELATED VALUES
+        //cannon isnt referenced here
+        bombMinTime = DV.WeaponRelated[2];
+
+        laserDamage = (int)DV.WeaponRelated[3];
+        laserMinTime = DV.WeaponRelated[4];
+        laserForce = DV.WeaponRelated[5];
+
+        stickyMinTime = DV.WeaponRelated[7];
+    }
+
     void OnDestroy()
     {
         GamePad.SetVibration((PlayerIndex)player, 0f, 0f);
